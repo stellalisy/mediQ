6da0eeff4b56a0aad9ddbb73165f899ac75b658f
stellali7 <sli136@jhu.edu>
2025-01-24 17:15:46 -0800

    fix typo in patient.py

--- src/patient.py
+++ src/patient.py
@@ -16,7 +16,8 @@ class Patient:
         else:
             self.initial_info = ""  # Default fallback
             self.context_list = []
-        
+            
+        self.model_name = args.patient_model
         self.history = []  # To track the interaction history of questions and answers
         self.facts = sample['atomic_facts'] if 'atomic_facts' in sample else None  # To store atomic facts after initial processing, you can choose to store this somewhere locally to avoid repeated processing
 
@@ -58,7 +59,7 @@ class DirectPatient(Patient):
         system_prompt = "Answer the question with the given context."
         user_prompt = f"Context: \"{self.initial_info}\"\nQuestion: \"{question}\"\n"
         messages = [{"role": "system", "content": system_prompt}, {"role": "user", "content": user_prompt}]
-        response = get_response(messages, max_length=50).strip()
+        response = get_response(messages, self.model_name, max_length=50).strip()
         self.update_state(question, response)
         return response
 
@@ -67,7 +68,7 @@ class InstructPatient(Patient):
         system_prompt = "You are a truthful assistant that understands the patient's information, and you are trying to answer questions from a medical doctor about the patient."
         user_prompt = f"Below is a context paragraph describing the patient and their conditions:\n\"{self.context_para}\"\nQuestion from the doctor: \"{question}\"\nUse the context paragraph to answer the doctor's question. If the paragraph does not answer the question, simply say \"The patient cannot answer this question, please do not ask this question again.\" Answer only what the question asks for. Do not provide any analysis, inference, or implications. Respond with a straightforward answer to the question ONLY and NOTHING ELSE."
         messages = [{"role": "system", "content": system_prompt}, {"role": "user", "content": user_prompt}]
-        response = get_response(messages, max_length=50)[0]['generated_text']
+        response = get_response(messages, self.model_name, max_length=50)[0]['generated_text']
         self.update_state(question, response)
         return response
     
@@ -78,11 +79,11 @@ class FactSelectPatient(Patient):
             system_prompt = "You are a truthful medical assistant that understands the patient's information."
             user_prompt = f"Break the following patient information into a list of independent atomic facts, with one piece of information in each statement. Each fact should only include the smallest unit of information, but should be self-contained.\n\"{self.context_para}\"\nResponse with the list of atomic facts and nothing else, prepend each fact by an index starting from 1. No sub-list allowed."
             messages = [{"role": "system", "content": system_prompt}, {"role": "user", "content": user_prompt}]
-            self.facts = get_response(messages, max_length=1000).splitlines()
+            self.facts = get_response(messages, self.model_name, max_length=1000).splitlines()
         
         facts_prompt = "\n".join(f"{i+1}. {fact}" for i, fact in enumerate(self.facts))
         system_prompt = f"List of facts:\n{facts_prompt}\n\nQuestion from the doctor: \"{question}\"\nWhich of the above facts answer the question?"
         messages = [{"role": "system", "content": system_prompt}]
-        response = get_response(messages, max_length=50)[0]['generated_text']
+        response = get_response(messages, self.model_name, max_length=50)[0]['generated_text']
         self.update_state(question, response)
         return response
